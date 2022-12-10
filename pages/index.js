aec1e042871dc01be27b16302874c3d6e0d2d703
AdityaAB4 <adityabarde4ab@gmail.com>
2022-12-10 15:57:37 +0530

    scroller hidden

--- pages/index.js
+++ pages/index.js
@@ -31,13 +31,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="black">
+      <main className="black h-screen">
         <style jsx>{`
           .black {
             background-color: #000000;
           }
         `}</style>
         <div className="flex  ">
+          {/* First Div */}
           <div className=" flex flex-col w-44">
             <div className="flex justify-end mr-6 mt-4  ">
               <BsTwitter className="text-slate-300  w-7 h-7  " />
@@ -69,10 +70,9 @@ export default function Home() {
               <CgMoreO className="text-slate-300  w-6 h-6  " />
             </div>
           </div>
-
-          <div className="bg-black text-slate-200 w-2/4 border-x border-slate-500">
-            <h1 className="m-2 font-bold">Home</h1>
-
+          {/* Second Div */}
+          <div className="bg-black text-slate-200 w-2/4 border-x border-slate-500  max-h-screen overflow-y-scroll scrollbar-hide">
+            <h1 className="m-2 font-bold text-lg">Home</h1>
             <div className="flex">
               <div className="m-3 ">
                 <CgProfile className="mt-2 h-10 w-10" />
@@ -90,7 +90,7 @@ export default function Home() {
                     Everyone can reply
                   </span>
                 </div>
-                <hr className=" " />
+                <hr className="h-px bg-gray-700 border-0 dark:bg-gray-700" />
                 <div className="flex flex-row  justify-between m-2">
                   <div className="flex flex-row justify-start">
                     <div className="mr-2 mt-2 ">
@@ -118,11 +118,11 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <hr />
+            <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
             <div className="flex flex-row m-4 text-blue-500  justify-center items-center">
               Show 411 Tweets
             </div>
-            <hr />
+            <hr className="h-px bg-gray-700 border-0 dark:bg-gray-700" />
             <div className="mt-4 mb-4">
               <div className="flex flex-row ">
                 <CgProfile className="w-10 h-10 ml-2 " />
@@ -142,7 +142,8 @@ export default function Home() {
                 Full list out in #MCWeekendReads⚡️tomorrow.
               </p>
             </div>
-            <hr />
+            <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+
             <div className="ml-16 mt-4 mb-4">
               <div className="flex flex-row"></div>
               <div className="flex flex-row ">
@@ -163,7 +164,7 @@ export default function Home() {
                 Full list out in #MCWeekendReads⚡️tomorrow.
               </p>
             </div>
-            <hr />
+            <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
             <div className="ml-16 mt-4 mb-4">
               <div className="flex flex-row ">
                 <span className="">Moneycontrol</span>
@@ -185,36 +186,15 @@ export default function Home() {
             </div>
             <hr />
           </div>
-          <div className="w-96">
-            <div className="relative text-white">
+          {/* Third Div */}
+          <div className="w-96 -mt-2 max-h-screen overflow-y-auto scrollbar-hide">
+            <div className="text-white">
               <input
                 type="search"
-                name="serch"
-                placeholder={`🔍 Search`}
-                className=" bg-gray-800 h-10 px-5 pr-10 m-5 rounded-full text-sm focus:outline-none"
+                name="search"
+                placeholder={`🔍 Search Twitter`}
+                className="h-10 px-5 pr-32 m-5 bg-gray-800 rounded-full text-sm focus:outline-none"
               />
-              <button
-                type="submit"
-                className="absolute right-0 top-0 mt-3 mr-4"
-              >
-                {/* <GoSearch className="w-4 h-4 p-24" /> */}
-                {/* <svg
-                  className="h-4 w-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  id="Capa_1"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 56.966 56.966"
-                  style="enable-background:new 0 0 56.966 56.966;"
-                  xml:space="preserve"
-                  width="512px"
-                  height="512px"
-                >
-                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                </svg> */}
-              </button>
             </div>
             <div className="px-5 py-5 mr-4 ml-4  w-10/12 bg-gray-800 text-slate-200 shadow-lg rounded-lg ">
               <span className="text-xl font-bold ">What&apos;s Happening </span>
